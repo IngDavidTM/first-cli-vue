5b044aea6aafa96a3ec96c5830b52b2f5a7f4cb3
IngDavidTM <david-1011@hotmail.com>
2024-05-06 16:17:35 -0500

    Add props to friend contact

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,13 @@
     <header><h1>My Friends</h1></header>
     <ul>
       <li>
-        <friend-contact></friend-contact>
-        <friend-contact></friend-contact>
+        <friend-contact
+          v-for="friend in friends"
+          :key="friend.id"
+          :name="friend.name"
+          :phone-number="friend.phone"
+          :email-address="friend.email"
+        ></friend-contact>
       </li>
     </ul>
   </section>
