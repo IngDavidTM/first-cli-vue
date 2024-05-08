8721bd3a143139ac800d469c6ad34b4d5306de78
IngDavidTM <david-1011@hotmail.com>
2024-05-08 08:37:14 -0500

    Add new friend component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <section>
     <header><h1>My Friends</h1></header>
+    <new-friend @add-contact="addContact" ></new-friend>
     <ul>
       <li>
         <friend-contact
@@ -46,6 +47,16 @@
           friend => friend.id === friendId
         );
         identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
+      },
+      addContact(contactData) {
+        const newFriend = {
+          id: new Date().toISOString(),
+          name: contactData.name,
+          phone: contactData.phone,
+          email: contactData.email,
+          isFavorite: false
+        };
+        this.friends.push(newFriend);
       }
     }
   }
@@ -83,7 +94,8 @@
     list-style: none;
   }
   
-  #app li {
+  #app li,
+  #app form {
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
     margin: 1rem auto;
     border-radius: 10px;
@@ -115,5 +127,19 @@
     background-color: #ec3169;
     border-color: #ec3169;
     box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+  }
+
+  #app input {
+    font: inherit;
+    padding: 0.15rem;
+  }
+  #app label {
+    font-weight: bold;
+    margin-right: 1rem;
+    width: 7rem;
+    display: inline-block;
+  }
+  #app form div {
+    margin: 1rem 0;
   }
 </style>
